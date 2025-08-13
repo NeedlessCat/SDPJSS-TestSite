20edd525fe3e45dc54918e8f9b8ebc0fe25ccb5c
NeedlessCat <kumar29.aayush@gmail.com>
2025-08-13 17:07:14 +0530

    reverted back

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,11 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/sdpjss`, {
-    ssl: true,
-    tlsAllowInvalidCertificates: false,
-    serverSelectionTimeoutMS: 5000,
-  });
+  await mongoose.connect(`${process.env.MONGODB_URI}/sdpjss`);
 };
 
 export default connectDB;
