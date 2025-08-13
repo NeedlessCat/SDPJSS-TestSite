6f30401e45590425eaa693a1cd71afd59db5649d
NeedlessCat <kumar29.aayush@gmail.com>
2025-08-13 17:01:27 +0530

    updated mongodb

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,7 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database Connected"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/sdpjss`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/sdpjss`, {
+    ssl: true,
+    tlsAllowInvalidCertificates: false,
+    serverSelectionTimeoutMS: 5000,
+  });
 };
 
 export default connectDB;
